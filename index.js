6dfde2fb6e70d44cad553face6e5b85f0a062f1f
Peter Peev <peterpeev28@gmail.com>
2022-10-31 18:22:56 +0200

    first react

---
+++ index.js
@@ -0,0 +1,4 @@
+// first react - 09:08
+ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"));
+
+ReactDOM.render(<label>Is this working?</label>, document.getElementById("root"));
